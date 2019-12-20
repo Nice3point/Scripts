bf9e35cf5510b68982a7d0f95199639fb1ab9f1d
Roman <nice3point@gmail.com>
2019-12-20 20:59:44 +0300

    Update Download NormyBy database.js

--- Download NormyBy database.js
+++ Download NormyBy database.js
@@ -1,20 +1,15 @@
 //wget -A pdf -m -p -E -k -K -np --no-check-certificate -P C:\Users\Roman.LIDER\Desktop\Normy https://normy.by/tnpa/1/
 
 function POSTreq() {
-  var full =[];
   for (i = 0; i < 88; i++) {
     (function(i){
       var xhr = new XMLHttpRequest();
-      var url = "https://normy.by/ips_list.php";
+      var url = "https://normy.by/fond_list.php";
       var params = "codename=&doctype%5B13%5D=on&doctype%5B14%5D=on&doctype%5B5%5D=on&doctype%5B6%5D=on&doctype%5B2%5D=on&doctype%5B9%5D=on&doctype%5B8%5D=on&doctype%5B10%5D=on&doctype%5B4%5D=on&code=&name=&datefrom=&dateto=&block=0&annotation=&kgs=&mks=&mode=false&page="+i;
       xhr.open('POST', url, false);
       xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
       xhr.onreadystatechange = function () {
-        if(xhr.readyState == 4) {
-          full.push(xhr.responseText);
-          full.join("");
-          if (i==87 || xhr.status == 400){download(full, "data"+i+".json", "text/plain");}
-        }
+      download(xhr.responseText, "data"+i+".json", "text/plain");
       };
       xhr.send(params);
     })(i);
@@ -44,3 +39,4 @@ function sleep(miliseconds) {
   }
 }
 POSTreq();
+
